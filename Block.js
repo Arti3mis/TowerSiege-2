63a2b9c927e801bb43b63a6f6365a54d4ac09c35
Arti3mis <75241631+Arti3mis@users.noreply.github.com>
2021-08-05 12:36:38 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,29 @@
+        class Block{
+    constructor(x,y,width,height,angle){
+    var options = {friction:0.5}
+    this.width = width; 
+    this.height = height;
+    this.body = Bodies.rectangle(x,y,width,height,options);   
+    this.visibility = 255;
+    World.add(world,this.body);
+        }
+    display(){
+    if(this.body.speed<5){
+    var angle = this.body.angle 
+    var pos = this.body.position;
+    push(); 
+    translate(pos.x,pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    rect(0,0,this.width,this.height);
+    pop();
+    }
+    else{
+         World.remove(world,this.body);
+         push();
+         this.visibility = this.visibility-5
+         pop();
+    }
+    }
+
+        }
